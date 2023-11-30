929e42d0f019b9f2e009d94c7f145cb36dbe29c4
Taruna Wahyudi <wahyuditaruna97@gmail.com>
2023-12-01 04:23:12 +0700

    create all entities

--- database.sql
+++ database.sql
@@ -1,3 +1,38 @@
-SELECT * FROM pg_user;
-
 CREATE DATABASE db_contact_management_restful_api;
+
+CREATE TABLE users (
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    token VARCHAR(100),
+    token_expired_at BIGINT,
+    PRIMARY KEY (username),
+    UNIQUE (token)
+);
+
+SELECT * FROM users;
+
+CREATE TABLE contacts (
+    id VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100),
+    phone VARCHAR(100),
+    email VARCHAR(100),
+    PRIMARY KEY (id),
+    CONSTRAINT fk_users_contacts FOREIGN KEY (username) REFERENCES users (username)
+);
+
+CREATE TABLE addresses (
+    id VARCHAR(100) NOT NULL,
+    contact_id VARCHAR(100) NOT NULL,
+    street VARCHAR(200),
+    city VARCHAR(100),
+    province VARCHAR(100),
+    country VARCHAR(100) NOT NULL,
+    postal_code VARCHAR(10),
+    PRIMARY KEY (id),
+    CONSTRAINT fk_contacts_addresses FOREIGN KEY (contact_id) REFERENCES contacts (id)
+);
+
+SELECT * FROM addresses;
